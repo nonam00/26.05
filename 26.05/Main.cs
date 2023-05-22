c12d596fd56aaa866ee18286c14b1d468972eb64
nonam00 <raiskt.ilya3@yandex.ru>
2023-05-22 18:12:59 +0300

    Добавьте файлы проекта.

---
+++ 26.05/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _26._05
+{
+    internal class Program : Task3
+    {
+        public static void Main(string[] args)
+        {
+            Do3();
+        }
+    }
+}
---
+++ 26.05/Percent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _26._05
+{
+    internal class Percent
+    {
+        private double value;
+        public double Value
+        {
+            get { return value; }
+            set 
+            {
+                if (value > 25)
+                    throw new Exception("Нельзя половжить вклад под процент больше 25\n");
+                else if (value <= 0)
+                    throw new Exception("Процент не может быть меньше или равен 0\n");
+                else
+                    this.value = value / 100 + 1;
+            }
+        }
+    }
+}
