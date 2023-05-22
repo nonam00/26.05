206cb64038fbb86dd526f59131c56bbb1ffce4fe
nonam00 <raiskt.ilya3@yandex.ru>
2023-05-22 20:37:40 +0300

    del

--- 26.05/Task.cs
+++ 26.05/Task.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _26._05
+﻿namespace _26._05
 {
     internal abstract class Task
     {
